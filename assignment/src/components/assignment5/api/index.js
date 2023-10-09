437553e0df37a8a4b6221c47a8d7d4ba01f70fbb
Ritesh Singh <risi1006@gmail.com>
2023-10-09 21:04:41 +0530

    added api token

--- assignment/src/components/assignment5/api/index.js
+++ assignment/src/components/assignment5/api/index.js
@@ -1,14 +1,16 @@
 import axios from 'axios';
-
 import { mockGiftData } from '../../../mock/assignment5';
+import {API_KEY} from '../../../credentials'
 export const fetchRecommendedGifts = async(body) => {
-    return mockGiftData;
+    // return mockGiftData;
     let data = await axios.post('https://api.openai.com/v1/chat/completions', body, {
     headers: {
         'Content-Type': 'application/json',
-        'Authorization': 'Bearer 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',
+        'Authorization': `Bearer ${API_KEY}`,
         'model': 'gpt-3.5-turbo'
     }
-    }).then(({data}) => {return JSON.parse(data?.choices[0]?.message?.content)});
+    }).then(({data}) => {return JSON.parse(data?.choices[0]?.message?.content)}).catch((e)=>{
+        return mockGiftData;
+    });
     return data;
 }
